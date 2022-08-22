88b42ac62562061d410de5754a0944ae89aead07
cmark0v <cmark0v@undef>
2022-08-22 09:18:50 +0900

    use command line arg instead of env var and re-add .env file useage

--- docker/test_w_rpc.sh
+++ docker/test_w_rpc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #run from repo root
-export RPC=http://127.0.0.1:8545
-docker run --network host -v $PWD:/app -e'FOUNDRY_ETH_RPC_URL=$RPC'  foundry "forge test --root /app --watch"
+export `grep ^RPC .env`
+docker run --network host -v $PWD:/app  foundry "forge test --root /app --watch --rpc-url $RPC"
