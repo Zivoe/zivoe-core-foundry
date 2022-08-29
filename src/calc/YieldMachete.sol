c3c06701469f2d6945ca62136ac100f615083af9
cmark0v <cmark0v@undef>
2022-08-29 16:13:15 +0900

    fixing

--- src/calc/YieldMachete.sol
+++ src/calc/YieldMachete.sol
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-/// @dev    YieldMachete.sol calculator for yield disection 
+/// @dev    YieldMachete.sol calculator for yield disection
 library YieldMachete {
-    uint256 constant ONE = 1 ether;//think its more gas efficient to regexp this out so its not stored in mem will do later
-    uint256 constant lookbackPeriod =13; //replace this with whatever it is in global gov, regexp or args into where it is needed
+    uint256 constant ONE = 1 ether; //think its more gas efficient to regexp this out so its not stored in mem will do later
+    uint256 constant lookbackPeriod = 13; //replace this with whatever it is in global gov, regexp or args into where it is needed
+
     function YieldTarget(
-        uint256 rateNow,
         uint256 seniorSupp,
         uint256 juniorSupp,
         uint256 targetRatio,
         uint256 yieldDelta
     ) internal pure returns (uint256) {
         uint256 dBig = 4 * yieldDelta;
-        return (targetRatio * seniorSupp + targetRatio * juniorSupp) / dBig;
+        return (targetRate * seniorSupp + targetRatio * targetRate * juniorSupp) / dBig;
     }
 
     function rateSenior(
         uint256 yieldBag,
         uint256 cumsumYield,
-        uint256 rateNow,
         uint256 seniorSupp,
         uint256 juniorSupp,
         uint256 targetRatio,
@@ -27,7 +26,7 @@ library YieldMachete {
     ) internal pure returns (uint256) {
         uint256 Y = YieldTarget(rateNow, seniorSupp, juniorSupp, targetRatio, yieldDelta);
         if (Y > yieldBag) {
-            return ONE * (dLil(targetRatio, juniorSupp, seniorSupp) / ONE);
+            return ONE / (dLil(targetRatio, juniorSupp, seniorSupp) / ONE);
         } else if (cumsumYield >= lookbackPeriod * Y) {
             return Y;
         } else {
@@ -59,5 +58,4 @@ library YieldMachete {
         //      m_s
         return ONE + (targetRatio * ONE * juniorSupp) / seniorSupp;
     }
-
 }
