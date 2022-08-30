907dd8405fc54a19f1b22a1a5a616716b78c15e5
JQ <johnrquarnstrom@gmail.com>
2022-08-30 12:37:43 -0700

    final tests

--- src/ZivoeGlobals.sol
+++ src/ZivoeGlobals.sol
@@ -5,7 +5,7 @@ import "./OpenZeppelin/Ownable.sol";
 
 /// @dev    This contract handles the global variables for the Zivoe protocol.
 contract ZivoeGlobals is Ownable {
-
+    
     // ---------------------
     //    State Variables
     // ---------------------
