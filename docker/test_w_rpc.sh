99c18eff3cd016c525b562a391c41570cb4eb228
cmark0v <cmark0v@undef>
2022-09-04 09:16:26 +0900

    foundry config file has nothing important going on. docker file is good to use --rm to keep it from taking up too much space and making a new container each run

--- docker/test_w_rpc.sh
+++ docker/test_w_rpc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #run from repo root
 export `grep ^RPC .env`
-docker run --network host -v $PWD:/app  foundry "forge test --root /app --watch --rpc-url $RPC"
+docker run --rm --network host -v $PWD:/app  foundry "forge test --root /app --watch --rpc-url $RPC"
