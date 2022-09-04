5fabd74703ac014ecebf8469fa2284cf7c03cdad
cmark0v <cmark0v@undef>
2022-09-04 13:50:14 +0900

    tests for default reg and response thereof

--- src/TESTS_Basic/ZivoeYDL.t.sol
+++ src/TESTS_Basic/ZivoeYDL.t.sol
@@ -6,21 +6,42 @@ import "./Utility.sol";
 contract ZivoeYDLTest is Utility {
     function setUp() public {
         setUpFundedDAO();
-        mint("FRAX", address(tom), 10 * 1 ether);
+        mint("FRAX", address(tom), 10000000 * 1 ether);
     }
 
     function test_stake_tokens_Half() public {
-        fundAndRepayBalloonLoan();
         stakeTokensHalf();
-    }
-
-    function test_stake_tokens_Full() public {
         fundAndRepayBalloonLoan();
+
+    }
+    function test_stake_tokens_Full() public {
         stakeTokensFull();
+        fundAndRepayBalloonLoan();
     }
 
-    function test_default_reg() public {
-        assert(god.try_registerDefault(address(YDL), 5 ether));
+    function test_steak_rewards_reg() public {
+
+        stakeTokensHalf();
+        uint256 bal1 = IERC20(FRAX).balanceOf(address(stSTT));
+
+        assert(tom.try_approveToken(address(FRAX), address(YDL), 60000 ether));
+        tom.try_passToTranchies(address(YDL),address(FRAX),50000 ether);
+        uint256 bal2 = IERC20(FRAX).balanceOf(address(stSTT));
+        assert(bal2>bal1);
+    }
+    function test_default_reg_response() public {
+        stakeTokensHalf();
+        assert(tom.try_approveToken(address(FRAX), address(YDL), 60000 ether));
+        uint256 Jbal1 = IERC20(address(zJTT)).balanceOf(address(stJTT));
+        uint256 bal1j = IERC20(address(FRAX)).balanceOf(address(stJTT));
+        tom.try_passToTranchies(address(YDL),address(FRAX),5000 ether);
+        uint256 bal2j = IERC20(address(FRAX)).balanceOf(address(stJTT));
+
+        assert(bal1j < bal2j);
+        assert(god.try_registerDefault(address(YDL), Jbal1));
+        tom.try_passToTranchies(address(YDL),address(FRAX),50000 ether);
+        uint256 bal2a = IERC20(address(FRAX)).balanceOf(address(stJTT));   
+        assert(bal2a < bal2j+50000);
     }
 
     function test_ZivoeYDL_passToTranchies() public {
