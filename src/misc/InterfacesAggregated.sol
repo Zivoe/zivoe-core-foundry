96125a966d9b308b070ba7190322dfc8d10ac2f3
JQ <johnrquarnstrom@gmail.com>
2022-09-15 10:58:45 -0700

    OCC_FRAX netDefaults updates

--- src/misc/InterfacesAggregated.sol
+++ src/misc/InterfacesAggregated.sol
@@ -14,28 +14,7 @@ interface IERC721 {
 }
 
 interface IERC1155 {
-    /**
-     * @dev Grants or revokes permission to `operator` to transfer the caller's tokens, according to `approved`,
-     *
-     * Emits an {ApprovalForAll} event.
-     *
-     * Requirements:
-     *
-     * - `operator` cannot be the caller.
-     */
     function setApprovalForAll(address operator, bool approved) external;
-
-    /**
-     * @dev xref:ROOT:erc1155.adoc#batch-operations[Batched] version of {safeTransferFrom}.
-     *
-     * Emits a {TransferBatch} event.
-     *
-     * Requirements:
-     *
-     * - `ids` and `amounts` must have the same length.
-     * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155BatchReceived} and return the
-     * acceptance magic value.
-     */
     function safeBatchTransferFrom(
         address from,
         address to,
@@ -80,6 +59,8 @@ interface IZivoeGlobals {
     function maxZVEPerJTTMint() external view returns (uint256);
     function lowerRatioIncentive() external view returns (uint256);
     function upperRatioIncentive() external view returns (uint256);
+    function increaseDefaults(uint256) external;
+    function decreaseDefaults(uint256) external;
 }
 
 interface IZivoeITO {
@@ -136,14 +117,7 @@ interface IUniswapV2Router01 {
     ) external returns (uint[] memory amounts);
 }
 
-
-// https://etherscan.io/address/0x8301ae4fc9c624d1d396cbdaa1ed877821d7c511#code (ETH/CRV)
-// https://etherscan.io/address/0xDC24316b9AE028F1497c275EB9192a3Ea0f67022#code (ETH/stETH)
 interface ICRVMetaPool {
-    // i = token_from
-    // j = token_to
-    // dx = token_from_change
-    // min_dy = token_to_min_receive
     function add_liquidity(uint256[2] memory amounts_in, uint256 min_mint_amount) external payable returns (uint256);
     function calc_withdraw_one_coin(uint256 _token_amount, int128 i) external view returns (uint256);
     function coins(uint256 i) external view returns (address);
@@ -152,7 +126,6 @@ interface ICRVMetaPool {
     function remove_liquidity_one_coin(uint256 token_amount, int128 index, uint min_amount) external;
 }
 
-// https://etherscan.io/address/0xDcEF968d416a41Cdac0ED8702fAC8128A64241A2#code (FRAX/USDC)
 interface ICRVPlainPoolFBP {
     function add_liquidity(uint256[2] memory amounts_in, uint256 min_mint_amount) external returns (uint256);
     function calc_withdraw_one_coin(uint256 _token_amount, int128 i) external view returns (uint256);
@@ -161,7 +134,6 @@ interface ICRVPlainPoolFBP {
     function remove_liquidity_one_coin(uint256 token_amount, int128 index, uint min_amount) external;
 }
 
-// https://etherscan.io/address/0xDcEF968d416a41Cdac0ED8702fAC8128A64241A2#code (FRAX/USDC)
 interface ICRVPlainPool3CRV {
     function add_liquidity(uint256[3] memory amounts_in, uint256 min_mint_amount) external;
     function coins(uint256 i) external view returns (address);
@@ -177,7 +149,6 @@ interface ICRV_MP_256 {
     function exchange_underlying(int128 i, int128 j, uint256 dx, uint256 min_dy) external payable returns (uint256);
 }
 
-// https://etherscan.io/address/0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f#code
 interface ISushiRouter {
      function addLiquidity(
         address tokenA,
@@ -240,9 +211,7 @@ interface IERC104 {
     function canPullERC1155() external view returns (bool);
 }
 
-/// @title Interface for making arbitrary calls during swap
 interface IAggregationExecutor {
-    /// @notice Make calls on `msgSender` with specified data
     function callBytes(address msgSender, bytes calldata data) external payable;  // 0x2636f7f8
 }
 
@@ -257,43 +226,17 @@ struct SwapDescription {
     bytes permit;
 }
 
-/// @title Interface for making arbitrary calls during swap
 interface IAggregationRouterV4 {
     function swap(IAggregationExecutor caller, SwapDescription memory desc, bytes calldata data) external payable;
 }
 
 interface ILendingPool {
-
-    /**
-    * @dev Deposits an `amount` of underlying asset into the reserve, receiving in return overlying aTokens.
-    * - E.g. User deposits 100 USDC and gets in return 100 aUSDC
-    * @param asset The address of the underlying asset to deposit
-    * @param amount The amount to be deposited
-    * @param onBehalfOf The address that will receive the aTokens, same as msg.sender if the user
-    *   wants to receive them on his own wallet, or a different address if the beneficiary of aTokens
-    *   is a different wallet
-    * @param referralCode Code used to register the integrator originating the operation, for potential rewards.
-    *   0 if the action is executed directly by the user, without any middle-man
-    **/
     function deposit(
         address asset,
         uint256 amount,
         address onBehalfOf,
         uint16 referralCode
     ) external;
-  
-
-    /**
-    * @dev Withdraws an `amount` of underlying asset from the reserve, burning the equivalent aTokens owned
-    * E.g. User has 100 aUSDC, calls withdraw() and receives 100 USDC, burning the 100 aUSDC
-    * @param asset The address of the underlying asset to withdraw
-    * @param amount The underlying amount to be withdrawn
-    *   - Send the value type(uint256).max in order to withdraw the whole aToken balance
-    * @param to Address that will receive the underlying, same as msg.sender if the user
-    *   wants to receive it on his own wallet, or a different address if the beneficiary is a
-    *   different wallet
-    * @return The final amount withdrawn
-    **/
     function withdraw(
         address asset,
         uint256 amount,
