0ddfb159d4a665d2ae1e932c0b815f3a60fde2fd
Alexandre Collette <alexcollette@hotmail.com>
2022-10-07 18:32:12 +0200

    staking + convexPoolId

--- src/lockers/OCY/OCY_CVX_module.sol
+++ src/lockers/OCY/OCY_CVX_module.sol
@@ -47,15 +47,19 @@ contract OCY_CVX_Module is ZivoeLocker, LockerSwapper {
     address public CRV3POOL;
 
     /// @dev If Metapool, provide following info:
+    /// note: will only work with metapools with 2 tokens: 1LP + 1 Base Token, LP_Underlying_Coins have to be in the same order as refered to on Curve (check pool.coins(0), pool.coins(1), etc)
     address public metapool;
+    address public METAPOOL_LP_TOKEN;
     address public BASE_TOKEN;
     address public BASE_LP_TOKEN;
-    address public LP_Pool_Origin;
-    address[] public LP_Pool_Coins;
+    address public LP_Underlying_Pool;
+    address[] public LP_Underlying_Coins;
+    uint256 public convexPoolID_mp;
 
     /// @dev If Plain Pool, both tokens are the following:
     /// note: at least one MP should exist for one of both tokens if we want public to be able to convert stablecoins.
     address public plainpool;
+    address public PLAINPOOL_LP_TOKEN;
     address public PP_TOKEN1;
     bool public MP1_exist;
     address public PP_TOKEN2;
@@ -66,6 +70,7 @@ contract OCY_CVX_Module is ZivoeLocker, LockerSwapper {
     address public MP2;
     /// The 3CRV lp token will always be set at second position
     address[] public tokensMP2;
+    uint256 public convexPoolID_pp;
 
     // -----------------
     //    Constructor
@@ -107,7 +112,16 @@ contract OCY_CVX_Module is ZivoeLocker, LockerSwapper {
         return true;
     }
 
-    function initPP(address _plainpool, address _PP_TOKEN1, bool _MP1_exist, address _PP_TOKEN2,  bool _MP2_exist, address _MP1, address _MP2) external {
+    function initPP(
+        address _plainpool,
+        address _PLAINPOOL_LP_TOKEN, 
+        address _PP_TOKEN1, 
+        bool _MP1_exist, 
+        address _PP_TOKEN2,  
+        bool _MP2_exist, 
+        address _MP1, 
+        address _MP2, 
+        uint256 _convexPoolID_pp) external {
         require(IZivoeGlobals(GBL).isKeeper(_msgSender()));
         require(PP_locker == true);
         require(PP_initialized == false);
@@ -116,25 +130,32 @@ contract OCY_CVX_Module is ZivoeLocker, LockerSwapper {
 
         if (_MP1_exist == true) {
             require(ICRVMetaPool(MP1).coins(0) == CRV3POOL || ICRVMetaPool(MP1).coins(1) == CRV3POOL);
-            require(ICRVMetaPool(MP1).coins(0) == _PP_TOKEN1 || ICRVMetaPool(MP1).coins(1) == __PP_TOKEN1);
+            require(ICRVMetaPool(MP1).coins(0) == _PP_TOKEN1 || ICRVMetaPool(MP1).coins(1) == _PP_TOKEN1);
         }
         if (_MP2_exist == true) {
             require(ICRVMetaPool(MP2).coins(0) == CRV3POOL || ICRVMetaPool(MP2).coins(1) == CRV3POOL);
-            require(ICRVMetaPool(MP2).coins(0) == _PP_TOKEN2 || ICRVMetaPool(MP2).coins(1) == __PP_TOKEN2);
+            require(ICRVMetaPool(MP2).coins(0) == _PP_TOKEN2 || ICRVMetaPool(MP2).coins(1) == _PP_TOKEN2);
         }        
 
         PP_initialized = true;
         plainpool = _plainpool;
+        PLAINPOOL_LP_TOKEN = _PLAINPOOL_LP_TOKEN;
         PP_TOKEN1 = _PP_TOKEN1;
         PP_TOKEN2 = _PP_TOKEN2;
         MP1_exist = _MP1_exist;
         MP2_exist = _MP2_exist;
         MP1 = _MP1;
         MP2 = _MP2;
-
+        convexPoolID_pp = _convexPoolID_pp;
     }
 
-    function initMP(address _metapool, address _BASE_TOKEN, address _BASE_LP_TOKEN, address[] memory _LP_Underlying_Coins) external {
+    function initMP(
+        address _metapool, 
+        address _METAPOOL_LP_TOKEN,
+        address _BASE_TOKEN, 
+        address _BASE_LP_TOKEN, 
+        address[] memory _LP_Underlying_Coins,
+        uint256 _convexPoolID_mp) external {
         require(IZivoeGlobals(GBL).isKeeper(_msgSender()));
         require(MP_locker == true);
         require(MP_initialized == false);
@@ -144,17 +165,19 @@ contract OCY_CVX_Module is ZivoeLocker, LockerSwapper {
 
         MP_initialized = true;
         metapool = _metapool;
+        METAPOOL_LP_TOKEN = _METAPOOL_LP_TOKEN;
         BASE_TOKEN = _BASE_TOKEN;
         BASE_LP_TOKEN = _BASE_LP_TOKEN;
-        LP_Pool_Origin = ICRVMetaPool(metapool).base_pool();
+        LP_Underlying_Pool = ICRVMetaPool(metapool).base_pool();
+        convexPoolID_mp = _convexPoolID_mp;
         //for (uint8 i = 0; i < 4; i++) {
-        //    require(ICRVPlainPoolFBP(LP_Pool_origin).coins(i) == _LP_Pool_Coins[0] )
+        //    require(ICRVPlainPoolFBP(LP_Underlying_Pool).coins(i) == _LP_Underlying_Coins[0] )
         //}
         uint8 coinsLength = _LP_Underlying_Coins.length;
         for (uint8 i = 0; i < coinsLength; i++) {
             ///
-            require(ICRVMetaPool(LP_Pool_Origin).coins(i) == _LP_Underlying_Coins[i]);
-            LP_Pool_Coins[i] = _LP_Underlying_Coins[i];
+            require(ICRVMetaPool(LP_Underlying_Pool).coins(i) == _LP_Underlying_Coins[i]);
+            LP_Underlying_Coins[i] = _LP_Underlying_Coins[i];
         }
         
     }
@@ -188,8 +211,8 @@ contract OCY_CVX_Module is ZivoeLocker, LockerSwapper {
         if (MP_locker == true) {
             /// We verify that the asset out is equal to one of the underlying tokens of the LP or the BASE_TOKEN.
             uint8 test;
-            for (uint8 i=0; i < LP_Pool_Coins.length; i++) {
-                if (LP_Pool_Coins[i] == assetOut) {
+            for (uint8 i=0; i < LP_Underlying_Coins.length; i++) {
+                if (LP_Underlying_Coins[i] == assetOut) {
                     test += 1;
                     break;
     
@@ -217,29 +240,53 @@ contract OCY_CVX_Module is ZivoeLocker, LockerSwapper {
         } 
 
         if (MP_locker == true) {
-            ///Check if we have coins that still needs to be deposited to get the LP token
-            uint8 length = LP_Pool_Coins.length;
+            ///Check if we have coins that still needs to be deposited to LP_Underlying_Pool to get the LP token
+            uint8 length = LP_Underlying_Coins.length;
             uint256[length] memory deposits_bp;
             uint8 test;
 
             for (uint8 i = 0; i < length; i++) {
-                uint256 tokenBalance = IERC20(LP_Pool_Coins[i].balanceOf(address(this)));
+                uint256 tokenBalance = IERC20(LP_Underlying_Coins[i].balanceOf(address(this)));
                 deposits_bp[i] = tokenBalance;
                 if (tokenBalance > 0) {
-                    IERC20(LP_Pool_Coins[i]).safeApprove(LP_Pool_Origin, tokenBalance);
+                    IERC20(LP_Underlying_Coins[i]).safeApprove(LP_Underlying_Pool, tokenBalance);
                 } else {
                     test += 1;
                 }
             }
             /// if test = length it means all amounts = 0 and no need to get the LP token.
             if (test < length) {
-                ICRVPlainPoolFBP(LP_Pool_Origin).add_liquidity(deposits_bp, 0);
+                ICRVPlainPoolFBP(LP_Underlying_Pool).add_liquidity(deposits_bp, 0);
             }
 
-            
+            uint256 coin0Balance = IERC20(ICRVMetaPool(metapool).coins(0)).balanceOf(address(this));
+            uint256 coin1Balance = IERC20(ICRVMetaPool(metapool).coins(1)).balanceOf(address(this));
+
+            if (coin0Balance > 0) {
+                IERC20(ICRVMetaPool(metapool).coins(0)).safeApprove(metapool, coin0Balance);
+            }
+            if (coin1Balance > 0) {
+                IERC20(ICRVMetaPool(metapool).coins(1)).safeApprove(metapool, coin1Balance);
+            }
+
+            address[2] memory deposits_mp = [coin0Balance, coin1Balance];
+            ICRVMetaPool(metapool).add_liquidity(deposits_mp, 0);
+
+
 
         }
 
+    }
+
+    /// @dev    This will stake total balance of LP tokens on Convex
+    /// @notice Private function, should only be called through invest().
+    function stakeLP() private {
+        if (MP_locker == true) {
+            IERC20(METAPOOL_LP_TOKEN).safeApprove(CVX_Deposit_Address, IERC20(lpFRAX_USDC).balanceOf(address(this)));
+            ICVX_Booster(CVX_Deposit_Address).depositAll(100, true);
+        }
+        IERC20(lpFRAX_USDC).safeApprove(CVX_Deposit_Address, IERC20(lpFRAX_USDC).balanceOf(address(this)));
+        ICVX_Booster(CVX_Deposit_Address).depositAll(100, true);
     }
 
 }
