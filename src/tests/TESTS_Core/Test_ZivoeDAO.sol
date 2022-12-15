6d28d741912ba2b44f9d7767f4b111621c3eb944
Alexandre Collette <alexcollette@hotmail.com>
2022-12-15 11:43:52 +0100

    undo previous commit

--- src/tests/TESTS_Core/Test_ZivoeDAO.sol
+++ src/tests/TESTS_Core/Test_ZivoeDAO.sol
@@ -929,6 +929,13 @@ contract Test_ZivoeDAO is Utility {
         uint256 amt_USDC = uint256(random) % IERC20(USDC).balanceOf(address(DAO));
         uint256 amt_USDT = uint256(random) % IERC20(USDT).balanceOf(address(DAO));
 
+        if (random < 1 * USD) {
+            amt_DAI += 1 ether;
+            amt_FRAX += 1 ether;
+            amt_USDC += 1 * USD;
+            amt_USDT += 1 * USD;
+        }
+
         address[] memory assets = new address[](4);
         uint256[] memory amounts = new uint256[](4);
         uint256[] memory amounts_partial = new uint256[](4);
