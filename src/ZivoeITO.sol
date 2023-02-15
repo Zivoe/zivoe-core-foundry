073044453cdc686868bae0b67fba7435ef996948
JQ <johnrquarnstrom@gmail.com>
2023-02-14 20:31:26 -0700

    ZivoeITO NatSpec + TODO

--- src/ZivoeITO.sol
+++ src/ZivoeITO.sol
@@ -7,9 +7,9 @@ import "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Met
 import "../lib/openzeppelin-contracts/contracts/utils/Context.sol";
 
 interface ITO_IERC20Mintable {
-    /// @notice Creates ERC20 tokens and assigns them to an address, increasing the total supply.
-    /// @param account The address to send the newly created tokens to.
-    /// @param amount The amount of tokens to create and send.
+    /// @notice Mints ERC20 token(s) for the provided account, increases totalSupply().
+    /// @param  account The address to mint tokens for.
+    /// @param  amount  The amount of tokens to mint.
     function mint(address account, uint256 amount) external;
 }
 
@@ -17,7 +17,7 @@ interface ITO_IZivoeGlobals {
     /// @notice Returns the address of the ZivoeDAO contract.
     function DAO() external view returns (address);
 
-    /// @notice Returns the address of the  ZivoeRewardsVesting ($ZVE) vesting contract.
+    /// @notice Returns the address of the ZivoeRewardsVesting ($ZVE) vesting contract.
     function vestZVE() external view returns (address);
 
     /// @notice Returns the address of the ZivoeYDL contract.
@@ -39,24 +39,24 @@ interface ITO_IZivoeGlobals {
     function ZVT() external view returns (address);
 
     /// @notice Handles WEI standardization of a given asset amount (i.e. 6 decimal precision => 18 decimal precision).
-    /// @param amount The amount of a given "asset".
-    /// @param asset The asset (ERC-20) from which to standardize the amount to WEI.
-    /// @return standardizedAmount The above amount standardized to 18 decimals.
+    /// @param  amount              The amount of a given "asset" to be standardized.
+    /// @param  asset               The asset (ERC-20) from which to standardize the amount to WEI.
+    /// @return standardizedAmount  The input amount, standardized to 18 decimals.
     function standardize(uint256 amount, address asset) external view returns (uint256 standardizedAmount);
 }
 
 interface ITO_IZivoeRewardsVesting {
     /// @notice Sets the vestingSchedule for an account.
-    /// @param  account The account vesting $ZVE.
-    /// @param  daysToCliff The number of days before vesting is claimable (a.k.a. cliff period).
-    /// @param  daysToVest The number of days for the entire vesting period, from beginning to end.
-    /// @param  amountToVest The amount of tokens being vested.
-    /// @param  revokable If the vested amount can be revoked.
+    /// @param  account         The account vesting $ZVE.
+    /// @param  daysToCliff     The number of days before vesting is claimable (a.k.a. cliff period).
+    /// @param  daysToVest      The number of days for the entire vesting period, from beginning to end.
+    /// @param  amountToVest    The amount of tokens being vested.
+    /// @param  revokable       If the vested amount can be revoked.
     function vest(address account, uint256 daysToCliff, uint256 daysToVest, uint256 amountToVest, bool revokable) external;
 }
 
 interface ITO_IZivoeTranches {
-    /// @notice Unlocks the ZivoeTranches contract for distributions, sets some initial variables.
+    /// @notice Unlocks the ZivoeTranches contract for distributions, sets initial variables.
     function unlock() external;
 }
 
@@ -66,9 +66,13 @@ interface ITO_IZivoeYDL {
 }
 
 /// @notice This contract will facilitate the Zivoe ITO ("Initial Tranche Offering").
-///         This contract will be permissioned by $zJTT and $zSTT to call mint().
-///         This contract will escrow 10% of $ZVE supply for ITO, claimable post-ITO.
-///         This contract will support claiming $ZVE based on proportionate amount of liquidity provided during the ITO.
+///         This contract has the following responsibilities:
+///          - Permissioned by $zJTT and $zSTT to call mint() when a user deposits.
+///          - Escrow $zJTT and $zSTT until the ITO concludes.
+///          - Facilitate claiming of $zJTT and $zSTT when the ITO concludes.
+///          - Vest $ZVE simulatenously during claiming (based on $pZVE credits).
+///          - Migrate deposits to the DAO after the ITO concludes.
+///          - Migrate a portion of deposits to ZVL after the ITO concludes.
 contract ZivoeITO is Context {
 
     using SafeERC20 for IERC20;
@@ -82,7 +86,7 @@ contract ZivoeITO is Context {
     
     address public immutable GBL;   /// @dev The ZivoeGlobals contract.
 
-    address[] public stables;       /// @dev Stablecoins allowed;
+    address[] public stables;       /// @dev Stablecoin(s) allowed for juniorDeposit() or seniorDeposit().
 
     bool public migrated;           /// @dev Identifies if ITO has migrated assets to the DAO.
 
@@ -92,6 +96,7 @@ contract ZivoeITO is Context {
     mapping(address => uint256) public seniorCredits;       /// @dev Tracks $pZVE (credits) an individual has from seniorDeposit().
 
     uint256 private constant operationAllocation = 1000;    /// @dev The amount (in BIPS) of ITO proceeds allocated for operations.
+
     uint256 private constant BIPS = 10000;
 
 
@@ -101,9 +106,9 @@ contract ZivoeITO is Context {
     // -----------------
 
     /// @notice Initializes the ZivoeITO contract.
-    /// @param _start The unix when the ITO will start.
-    /// @param _end The unix when the ITO will end (airdrop is claimable).
-    /// @param _GBL The ZivoeGlobals contract.
+    /// @param _start   The unix when the ITO will start.
+    /// @param _end     The unix when the ITO will end (airdrop is claimable).
+    /// @param _GBL     The ZivoeGlobals contract.
     /// @param _stables Array of stablecoins representing initial stablecoin inputs.
     constructor (uint256 _start, uint256 _end, address _GBL, address[] memory _stables) {
         require(_start < _end, "ZivoeITO::constructor() _start >= _end");
@@ -120,33 +125,33 @@ contract ZivoeITO is Context {
     // ------------
 
     /// @notice Emitted during depositJunior().
-    /// @param  account The account depositing stablecoins to junior tranche.
-    /// @param  asset The stablecoin deposited.
-    /// @param  amount The amount of stablecoins deposited.
-    /// @param  credits The amount of credits earned.
-    /// @param  trancheTokens The amount of Zivoe Junior Tranche ($zJTT) tokens minted.
+    /// @param  account         The account depositing stablecoins to junior tranche.
+    /// @param  asset           The stablecoin deposited.
+    /// @param  amount          The amount of stablecoins deposited.
+    /// @param  credits         The amount of credits earned.
+    /// @param  trancheTokens   The amount of Zivoe Junior Tranche ($zJTT) tokens minted.
     event JuniorDeposit(address indexed account, address indexed asset, uint256 amount, uint256 credits, uint256 trancheTokens);
 
     /// @notice Emitted during depositSenior().
-    /// @param  account The account depositing stablecoins to senior tranche.
-    /// @param  asset The stablecoin deposited.
-    /// @param  amount The amount of stablecoins deposited.
-    /// @param  credits The amount of credits earned.
-    /// @param  trancheTokens The amount of Zivoe Senior Tranche ($zSTT) tokens minted.
+    /// @param  account         The account depositing stablecoins to senior tranche.
+    /// @param  asset           The stablecoin deposited.
+    /// @param  amount          The amount of stablecoins deposited.
+    /// @param  credits         The amount of credits earned.
+    /// @param  trancheTokens   The amount of Zivoe Senior Tranche ($zSTT) tokens minted.
     event SeniorDeposit(address indexed account, address indexed asset, uint256 amount, uint256 credits, uint256 trancheTokens);
 
     /// @notice Emitted during claim().
-    /// @param  account The account withdrawing stablecoins from senior tranche.
+    /// @param  account     The account withdrawing stablecoins from senior tranche.
     /// @param  zSTTClaimed The amount of Zivoe Senior Tranche ($zSTT) tokens received.
     /// @param  zJTTClaimed The amount of Zivoe Junior Tranche ($zJTT) tokens received.
-    /// @param  ZVEClaimed The amount of Zivoe ($ZVE) tokens received.
+    /// @param  ZVEClaimed  The amount of Zivoe ($ZVE) tokens received.
     event AirdropClaimed(address indexed account, uint256 zSTTClaimed, uint256 zJTTClaimed, uint256 ZVEClaimed);
 
     /// @notice Emitted during migrateDeposits().
-    /// @param  DAI Total amount of DAI migrated from the ITO to the DAO and ZVL.
-    /// @param  FRAX Total amount of FRAX migrated from the ITO to the DAO and ZVL.
-    /// @param  USDC Total amount of USDC migrated from the ITO to the DAO and ZVL.
-    /// @param  USDT Total amount of USDT migrated from the ITO to the DAO and ZVL.
+    /// @param  DAI     Total amount of DAI migrated from the ITO to the DAO and ZVL.
+    /// @param  FRAX    Total amount of FRAX migrated from the ITO to the DAO and ZVL.
+    /// @param  USDC    Total amount of USDC migrated from the ITO to the DAO and ZVL.
+    /// @param  USDT    Total amount of USDT migrated from the ITO to the DAO and ZVL.
     event DepositsMigrated(uint256 DAI, uint256 FRAX, uint256 USDC, uint256 USDT);
 
 
@@ -158,7 +163,7 @@ contract ZivoeITO is Context {
     /// @notice Claim $zSTT, $zJTT, and $ZVE after ITO concludes.
     /// @return zSTTClaimed Amount of $zSTT airdropped.
     /// @return zJTTClaimed Amount of $zJTT airdropped.
-    /// @return ZVEClaimed Amount of $ZVE airdropped.
+    /// @return ZVEClaimed  Amount of $ZVE airdropped.
     function claim() external returns (uint256 zSTTClaimed, uint256 zJTTClaimed, uint256 ZVEClaimed) {
         require(block.timestamp > end || migrated, "ZivoeITO::claim() block.timestamp <= end && !migrated");
 
@@ -194,8 +199,7 @@ contract ZivoeITO is Context {
         return ( seniorCreditsOwned / 3, juniorCreditsOwned, upper * middle / lower);
     }
 
-    /// @notice Deposit stablecoins into the junior tranche.
-    ///         Mints Zivoe Junior Tranche ($zJTT) tokens and increases airdrop credits.
+    /// @notice Deposit stablecoins into the junior tranche. Mints Zivoe Junior Tranche ($zJTT) tokens and increases airdrop credits.
     /// @param  amount The amount to deposit.
     /// @param  asset The asset to deposit.
     function depositJunior(uint256 amount, address asset) external { 
@@ -219,8 +223,7 @@ contract ZivoeITO is Context {
         ITO_IERC20Mintable(ITO_IZivoeGlobals(GBL).zJTT()).mint(address(this), standardizedAmount);
     }
 
-    /// @notice Deposit stablecoins into the senior tranche.
-    ///         Mints Zivoe Senior Tranche ($zSTT) tokens and increases airdrop credits.
+    /// @notice Deposit stablecoins into the senior tranche. Mints Zivoe Senior Tranche ($zSTT) tokens and increases airdrop credits.
     /// @param  amount The amount to deposit.
     /// @param  asset The asset to deposit.
     function depositSenior(uint256 amount, address asset) external {
@@ -243,6 +246,8 @@ contract ZivoeITO is Context {
         IERC20(asset).safeTransferFrom(caller, address(this), amount);
         ITO_IERC20Mintable(ITO_IZivoeGlobals(GBL).zSTT()).mint(address(this), standardizedAmount);
     }
+
+    // TODO: Helper function for accepting deposits.
 
     /// @notice Migrate tokens to DAO post-ITO.
     /// @dev    Only callable when block.timestamp > _concludeUnix.
