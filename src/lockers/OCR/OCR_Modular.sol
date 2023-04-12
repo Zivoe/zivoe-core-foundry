b93813acc30eb79f8c0efab0db61610fe5329587
Alexandre Collette <alexcollette@hotmail.com>
2023-04-12 17:35:42 +0200

    OCR redemption request functions

--- src/lockers/OCR/OCR_Modular.sol
+++ src/lockers/OCR/OCR_Modular.sol
@@ -2,6 +2,17 @@
 pragma solidity ^0.8.16;
 
 import "../../ZivoeLocker.sol";
+import "../../../lib/openzeppelin-contracts/contracts/utils/Context.sol";
+
+/// Note: 
+/// -Should we have separate claim timestamps for junior and senior ?
+
+interface IZivoeGlobals_OCR {
+    /// @notice Returns the address of the $zSTT contract.
+    function zSTT() external view returns (address);
+    /// @notice Returns the address of the $zJTT contract.
+    function zJTT() external view returns (address);
+}
 
 /// @notice  OCR stands for "On-Chain Redemption".
 ///          This locker is responsible for handling redemptions of tranche tokens to stablecoins.
@@ -13,10 +24,18 @@ contract OCC_Modular is ZivoeLocker {
     //    State Variables
     // ---------------------
 
-    uint16 public redemptionFee;             /// @dev Redemption fee on withdrawals via OCR (in BIPS)
-    address public immutable stablecoin;     /// @dev The stablecoin redeemable in this contract
-    address public immutable GBL;            /// @dev The ZivoeGlobals contract.    
-    
+    uint16 public redemptionFee;             /// @dev Redemption fee on withdrawals via OCR (in BIPS).
+    address public immutable stablecoin;     /// @dev The stablecoin redeemable in this contract.
+    address public immutable GBL;            /// @dev The ZivoeGlobals contract.   
+    uint256 public totalRedemptionsOnPeriod; /// @dev total amount of redemption requests for next distribution.
+    uint256 public lastDistribution;         /// @dev Used for timelock constraint for redemptions.
+
+    /// @dev Mapping of an address to a specific timestamp.   
+    mapping (address => uint256) public userClaimTimestamp;   
+    /// @dev Contains $zJTT token balance of each account (is 1:1 ratio with amount deposited)
+    mapping(address => uint256) private juniorBalances;
+    /// @dev Contains $zSTT token balance of each account (is 1:1 ratio with amount deposited).
+    mapping(address => uint256) private seniorBalances; 
 
 
     // -----------------
@@ -47,6 +66,24 @@ contract OCC_Modular is ZivoeLocker {
     /// @notice Permission for owner to call pullFromLocker().
     function canPull() public override pure returns (bool) { return true; }
 
+    /// @notice Initiates a redemption request for junior tranche tokens
+    /// @param amount The amount of junior tranche tokens to redeem
+    function redemptionRequestJunior(uint256 amount) external {
+        require(IERC20(IZivoeGlobals_OCR(GBL).zJTT()).balanceOf(_msgSender()) >= amount,
+        "OCR_Modular::redemptionRequestJunior() balanceOf(_msgSender()) < amount");
+        IERC20(IZivoeGlobals_OCR(GBL).zJTT()).safeTransferFrom(_msgSender(), address(this), amount);
+        juniorBalances[_msgSender()] += amount;
+        userClaimTimestamp[_msgSender()] = block.timestamp;
+    }
 
+    /// @notice Initiates a redemption request for senior tranche tokens
+    /// @param amount The amount of senior tranche tokens to redeem
+    function redemptionRequestSenior(uint256 amount) external {
+        require(IERC20(IZivoeGlobals_OCR(GBL).zSTT()).balanceOf(_msgSender()) >= amount,
+        "OCR_Modular::redemptionRequestSenior() balanceOf(_msgSender()) < amount");
+        IERC20(IZivoeGlobals_OCR(GBL).zSTT()).safeTransferFrom(_msgSender(), address(this), amount);
+        seniorBalances[_msgSender()] += amount;
+        userClaimTimestamp[_msgSender()] = block.timestamp;
+    }
 
 }
