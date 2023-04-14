fb113e2c7a01ebc18f0259ed6148f85b3ca656f9
pseudonaut <jquarnstrom@yahoo.com>
2023-04-14 12:25:26 -0700

    Pragma upgrade

--- src/ZivoeTrancheToken.sol
+++ src/ZivoeTrancheToken.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.16;
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.17;
 
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
