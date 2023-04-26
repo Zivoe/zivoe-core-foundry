96b6fd63cd59825c742f7a4330a59d0d5a3c3e74
Alexandre Collette <alexcollette@hotmail.com>
2023-04-26 14:38:28 +0200

    balance requirement to avoid over/underflow error

--- src/lockers/OCR/OCR_Modular.sol
+++ src/lockers/OCR/OCR_Modular.sol
@@ -177,6 +177,10 @@ contract OCR_Modular is ZivoeLocker, ReentrancyGuard {
             asset != OCR_IZivoeGlobals(GBL).zSTT(),
             "OCR_Modular::pullFromLockerPartial() asset == zJTT || asset == zSTT"
         );
+        require(
+            amount <= IERC20(asset).balanceOf(address(this)),
+            "OCR_Modular::pullFromLockerPartial() amount > IERC20(asset).balanceOf(address(this))"
+        );
 
         if (amount > amountRedeemableQueued && asset == stablecoin) {
             amountRedeemable -= (amount - amountRedeemableQueued);
