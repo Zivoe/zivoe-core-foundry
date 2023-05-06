6ce95f01c35cc1e84f4c4373528bcab7976d3159
pseudonaut <jquarnstrom@yahoo.com>
2023-05-05 19:33:38 -0700

    OCR v2

--- src/lockers/OCR/OCR_ModularV2.sol
+++ src/lockers/OCR/OCR_ModularV2.sol
@@ -7,6 +7,9 @@ import "../../../lib/openzeppelin-contracts/contracts/security/ReentrancyGuard.s
 import "../../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 interface IZivoeGlobals_OCR {
+    /// @notice Tracks net defaults in the system.
+    function defaults() external view returns (uint256);
+
     /// @notice Returns the address of the Timelock contract.
     function TLC() external view returns (address);
 
@@ -20,19 +23,17 @@ interface IZivoeGlobals_OCR {
     /// @return zSTTSupply zSTT.totalSupply() adjusted for defaults.
     /// @return zJTTSupply zJTT.totalSupply() adjusted for defaults.
     function adjustedSupplies() external view returns (uint256 zSTTSupply, uint256 zJTTSupply);
-
-    /// @notice Returns true if an address is whitelisted as a keeper.
-    /// @return keeper Equals "true" if address is a keeper, "false" if not.
-    function isKeeper(address) external view returns (bool keeper);
     
     /// @notice Handles WEI standardization of a given asset amount (i.e. 6 decimal precision => 18 decimal precision).
     /// @param  amount The amount of a given "asset".
     /// @param  asset The asset (ERC-20) from which to standardize the amount to WEI.
     /// @return standardizedAmount The above amount standardized to 18 decimals.
     function standardize(uint256 amount, address asset) external view returns (uint256 standardizedAmount);
+}
 
-    /// @notice Burns $zTT tokens.
-    /// @param  amount The number of $zTT tokens to burn.
+interface IERC20Burnable {
+    /// @notice Burns tokens.
+    /// @param  amount The number of tokens to burn.
     function burn(uint256 amount) external;
 }
 
@@ -46,7 +47,7 @@ contract OCR_ModularV2 is ZivoeLocker, ReentrancyGuard {
         address account;        /// @dev The account making the request.
         uint256 amount;         /// @dev The amount of the request ($zSTT or $zJTT).
         uint256 unlocks;        /// @dev The timestamp after which this request may be processed.
-        bool seniorOrJunior;    /// @dev The tranche this request is for (true = Senior, false = Junior).
+        bool seniorElseJunior;  /// @dev The tranche this request is for (true = Senior, false = Junior).
     }
 
     // ---------------------
@@ -60,34 +61,21 @@ contract OCR_ModularV2 is ZivoeLocker, ReentrancyGuard {
     
     uint256 public redemptionsFee;                  /// @dev Fee for redemptions (in BIPS).
 
-    uint256 public redemptionsAllowedJunior;
-    uint256 public redemptionsAllowedSenior;
+    uint256 public redemptionsAllowedJunior;        /// @dev Redemptions allowed for $zJTT (junior tranche).
+    uint256 public redemptionsAllowedSenior;        /// @dev Redemptions allowed for $zSTT (senior tranche).
 
-    uint256 public redemptionsQueuedJunior;
-    uint256 public redemptionsQueuedSenior;
+    uint256 public redemptionsQueuedJunior;         /// @dev Redemptions queued for $zJTT (junior tranche).
+    uint256 public redemptionsQueuedSenior;         /// @dev Redemptions queued for $zSTT (senior tranche).
 
-    uint256 public epochDiscountJunior;
-    uint256 public epochDiscountSenior;
+    uint256 public epochDiscountJunior;             /// @dev Redemption discount for $zJTT (junior tranche).
+    uint256 public epochDiscountSenior;             /// @dev Redemption discount for $zSTT (senior tranche).
 
-    /*
-        rAS = 1,000,000
-        rAJ = 1,000,000
-        eDJ = 1000
-        eDS = 0
-
-        rAS = 1,000,000
-        rAJ = 1,000,000
-        eDJ = 0
-        eDS = 0
-
-        burnS == sB / (rAS + rAJ)
-    */
-
-    uint256 public epoch;                           /// @dev The timestamp of this epoch.
+    uint256 public epoch;                           /// @dev The timestamp of current epoch.
 
     mapping(uint256 => Request) public requests;    /// @dev Mapping of all requests.
 
     uint256 private constant BIPS = 10000;
+    uint256 private constant RAY = 10**27;
 
 
 
@@ -114,17 +102,28 @@ contract OCR_ModularV2 is ZivoeLocker, ReentrancyGuard {
     //    Events
     // ------------
 
-    /// @notice Emitted during tickEpoch() and _tickEpoch().
-    event EpochTicked();
+    /// @notice Emitted during tickEpoch().
+    /// @param  epoch The timestamp of the start of this epoch.
+    /// @param  redemptionsAllowedJunior Redemptions allowed for $zJTT (junior tranche) for this epoch.
+    /// @param  redemptionsAllowedSenior Redemptions allowed for $zSTT (senior tranche) for this epoch.
+    /// @param  epochDiscountJunior Redemption discount for $zJTT (junior tranche) for this epoch.
+    /// @param  epochDiscountSenior Redemption discount for $zSTT (senior tranche) for this epoch.
+    event EpochTicked(
+        uint256 epoch, 
+        uint256 redemptionsAllowedJunior, 
+        uint256 redemptionsAllowedSenior,
+        uint256 epochDiscountJunior, 
+        uint256 epochDiscountSenior
+    );
 
     /// @notice Emitted during createRequest().
-    event RequestCreated();
+    event RequestCreated(uint256 indexed id, address indexed account, uint256 amount, bool indexed seniorElseJunior);
 
     /// @notice Emitted during destroyRequest().
-    event RequestDestroyed();
+    event RequestDestroyed(uint256 indexed id, address indexed account, uint256 amount, bool indexed seniorElseJunior);
 
     /// @notice Emitted during processRequest().
-    event RequestProcessed();
+    event RequestProcessed(uint256 indexed id, address indexed account, uint256 burnAmount, uint256 redeemAmount, bool indexed seniorElseJunior);
 
     /// @notice Emitted during updateRedemptiosnFee().
     /// @param  oldFee The old value of redemptionFee.
@@ -172,8 +171,7 @@ contract OCR_ModularV2 is ZivoeLocker, ReentrancyGuard {
     /// @param  data Accompanying transaction data.
     function pullFromLocker(address asset, bytes calldata data) external override _tickEpoch onlyOwner nonReentrant {
         require(
-            asset != IZivoeGlobals_OCR(GBL).zJTT() &&
-            asset != IZivoeGlobals_OCR(GBL).zSTT(),
+            asset != IZivoeGlobals_OCR(GBL).zJTT() && asset != IZivoeGlobals_OCR(GBL).zSTT(),
             "OCR_Modular::pullFromLocker() asset == zJTT || asset == zSTT"
         );
         IERC20(asset).safeTransfer(owner(), IERC20(asset).balanceOf(address(this)));
@@ -185,11 +183,75 @@ contract OCR_ModularV2 is ZivoeLocker, ReentrancyGuard {
     /// @param  data Accompanying transaction data.
     function pullFromLockerPartial(address asset, uint256 amount, bytes calldata data) external override _tickEpoch onlyOwner nonReentrant {
         require(
-            asset != IZivoeGlobals_OCR(GBL).zJTT() &&
-            asset != IZivoeGlobals_OCR(GBL).zSTT(),
+            asset != IZivoeGlobals_OCR(GBL).zJTT() && asset != IZivoeGlobals_OCR(GBL).zSTT(),
             "OCR_Modular::pullFromLockerPartial() asset == zJTT || asset == zSTT"
         );
         IERC20(asset).safeTransfer(owner(), amount);
+    }
+
+    /// @notice Creates a redemption request.
+    /// @param  amount The amount to deposit for the request.
+    /// @param  seniorElseJunior The tranche to deposit for (true = Senior, false = Junior).
+    function createRequest(uint256 amount, bool seniorElseJunior) public _tickEpoch {
+        emit RequestCreated(requestCounter, _msgSender(), amount, seniorElseJunior);
+        requests[requestCounter] = Request(_msgSender(), amount, epoch + 14 days, seniorElseJunior);
+        if (seniorElseJunior) {
+            redemptionsQueuedSenior += amount;
+            IERC20(IZivoeGlobals_OCR(GBL).zSTT()).safeTransferFrom(owner(), address(this), amount);
+        }
+        else {
+            redemptionsQueuedJunior += amount;
+            IERC20(IZivoeGlobals_OCR(GBL).zJTT()).safeTransferFrom(owner(), address(this), amount);
+        }
+        requestCounter += 1;
+    }
+
+    /// @notice Destroys a redemption request.
+    /// @param  id The ID of the request to destroy.
+    function destroyRequest(uint256 id) public _tickEpoch {
+        require(requests[id].account == _msgSender(), "OCR_Modular::destroyRequest() requests[id].account != _msgSender()");
+        require(requests[id].amount > 0, "OCR_Modular::destroyRequest() requests[id].amount == 0");
+        emit RequestDestroyed(id, requests[id].account, requests[id].amount, requests[id].seniorElseJunior);
+        if (requests[id].seniorElseJunior) {
+            IERC20(IZivoeGlobals_OCR(GBL).zSTT()).safeTransfer(requests[id].account, requests[id].amount);
+            if (requests[id].unlocks > epoch) { redemptionsQueuedSenior -= requests[id].amount; }
+            else { redemptionsAllowedSenior -= requests[id].amount; }
+        }
+        else {
+            IERC20(IZivoeGlobals_OCR(GBL).zJTT()).safeTransfer(requests[id].account, requests[id].amount);
+            if (requests[id].unlocks > epoch) { redemptionsQueuedJunior -= requests[id].amount; }
+            else { redemptionsAllowedJunior -= requests[id].amount; }
+        }
+        requests[id].amount = 0;
+    }
+
+    /// @notice Processes a redemption request.
+    /// @param  id The ID of the request to destroy.
+    function processRequest(uint256 id) public _tickEpoch {
+        require(requests[id].amount > 0, "OCR_Modular::destroyRequest() requests[id].amount == 0");
+        uint256 totalRedemptions = redemptionsAllowedSenior * (BIPS - epochDiscountSenior) + redemptionsAllowedJunior * (BIPS - epochDiscountJunior);
+        
+        if (totalRedemptions > 0) {
+            uint256 portion = (IERC20(stablecoin).balanceOf(address(this)) * RAY / totalRedemptions) / 10**23;
+            if (portion > BIPS) { portion = BIPS; }
+            uint256 burnAmount = requests[id].amount * portion / BIPS;
+            requests[id].amount -= burnAmount;
+            uint256 redeemAmount;
+            if (requests[id].seniorElseJunior) {
+                IERC20Burnable(IZivoeGlobals_OCR(GBL).zSTT()).burn(burnAmount);
+                redeemAmount = burnAmount * (10000 - epochDiscountSenior) / BIPS;
+            }
+            else {
+                IERC20Burnable(IZivoeGlobals_OCR(GBL).zJTT()).burn(burnAmount);
+                redeemAmount = burnAmount * (10000 - epochDiscountJunior) / BIPS;
+            }
+            if (IERC20Metadata(stablecoin).decimals() < 18) {
+                redeemAmount /= 10 ** (18 - IERC20Metadata(stablecoin).decimals());
+            }
+            IERC20(stablecoin).transfer(requests[id].account, redeemAmount * redemptionsFee / BIPS);
+            IERC20(stablecoin).transfer(requests[id].account, redeemAmount * (10000 - redemptionsFee) / BIPS );
+            emit RequestProcessed(id, requests[id].account, burnAmount, redeemAmount, requests[id].seniorElseJunior);
+        }
     }
 
     /// @notice Ticks the epoch.
@@ -200,8 +262,19 @@ contract OCR_ModularV2 is ZivoeLocker, ReentrancyGuard {
             redemptionsAllowedSenior += redemptionsQueuedSenior;
             redemptionsQueuedJunior = 0;
             redemptionsQueuedSenior = 0;
-            // TODO: epochDiscountJunior
-            // TODO: epochDiscountSenior
+            uint256 totalDefaults = IZivoeGlobals_OCR(GBL).defaults();
+            uint256 zSTTSupply = IERC20(IZivoeGlobals_OCR(GBL).zSTT()).totalSupply();
+            uint256 zJTTSupply = IERC20(IZivoeGlobals_OCR(GBL).zJTT()).totalSupply();
+            if (totalDefaults > zJTTSupply) {
+                epochDiscountJunior = BIPS;
+                totalDefaults -= zJTTSupply;
+                epochDiscountSenior = (totalDefaults * RAY / zSTTSupply) / 10**23;
+            }
+            else {
+                epochDiscountJunior = (totalDefaults * RAY / zJTTSupply) / 10**23;
+            }
+            emit EpochTicked(epoch, redemptionsAllowedJunior, redemptionsAllowedSenior, epochDiscountJunior, epochDiscountSenior);
+            tickEpoch(); /// @dev Recursive (in case multiple epochs have passed).
         }
     }
 
