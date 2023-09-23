bbb05a42e2b7be73287d3cb987cf07ce11cf8109
pseudonaut <jquarnstrom@yahoo.com>
2023-09-23 09:07:43 -0500

    YDL Spelling

--- src/ZivoeYDL.sol
+++ src/ZivoeYDL.sol
@@ -388,7 +388,7 @@ contract ZivoeYDL is Context, ReentrancyGuard {
     /// @notice Updates the protocolRecipients or residualRecipients.
     /// @param  recipients An array of addresses to which protocol earnings will be distributed.
     /// @param  proportions An array of ratios relative to the recipients - in BIPS. Sum should equal to 10000.
-    /// @param  protocol Specifiy "true" to update protocol earnings, or "false" to update residual earnings.
+    /// @param  protocol Specify "true" to update protocol earnings, or "false" to update residual earnings.
     function updateRecipients(address[] memory recipients, uint256[] memory proportions, bool protocol) external {
         require(_msgSender() == IZivoeGlobals_YDL(GBL).TLC(), "ZivoeYDL::updateRecipients() _msgSender() != TLC()");
         require(
