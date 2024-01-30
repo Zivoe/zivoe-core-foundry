f664bebfe11fb302aad376c914049633792396c2
pseudonaut <jquarnstrom@yahoo.com>
2024-01-29 20:39:46 -0500

    Presale Scaffolding

---
+++ src/misc/Presale.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/// @notice This contract facilitates a presale for Zivoe.
+contract Presale {
+
+    using SafeERC20 for IERC20;
+
+}
